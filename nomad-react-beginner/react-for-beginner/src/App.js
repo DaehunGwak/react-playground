b1fb532318c56c16214d94204f9b0e3ecca9e2e4
DaehunGwak <rhkrgns56@naver.com>
2024-02-18 02:04:15 +0900

    nomad-movie: effects - 컴포넌트에서 한번만 실행되는 useEffect
    - 처음에만 state 변화에 컴포넌트가 리렌더되고 그 이후에는 변화는 리렌더링 되고 싶지 않다면? > useEffect

--- nomad-react-beginner/react-for-beginner/src/App.js
+++ nomad-react-beginner/react-for-beginner/src/App.js
@@ -1,11 +1,27 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [result, setResult] = useState("doing...");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API...");
+    setTimeout(() => {
+      setResult("done!");
+      console.log("DONE");
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <h3>{result}</h3>
     </div>
   );
 }
