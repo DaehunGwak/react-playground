fc769932321f9effedab5b7b551d4cb00b91207d
DaehunGwak <rhkrgns56@naver.com>
2024-02-19 01:49:40 +0900

    nomad-movie: effects - 컴포넌트 destroyed 시 실행되는 코드 추가

--- nomad-react-beginner/react-for-beginner/src/App.js
+++ nomad-react-beginner/react-for-beginner/src/App.js
@@ -1,13 +1,23 @@
 import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destroyed");
+  }, []);
+  return (<h1>Hello</h1>);
+}
+
 function App() {
   const [counter, setCounter] = useState(0);
   const [result, setResult] = useState("doing...");
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
   const onClick = () => setCounter((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onShowingClick = () => setShowing((prev) => !prev);
 
   console.log("i run all the time");
 
@@ -42,6 +52,8 @@ function App() {
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
       <h3>{result}</h3>
+      {showing ? <Hello/> : null}
+      <button onClick={onShowingClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
