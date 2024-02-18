48a3fe33cf8cc145935039cd7f59a69d80fd4807
DaehunGwak <rhkrgns56@naver.com>
2024-02-18 18:00:08 +0900

    nomad-movie: effects - useEffects deps 활용한 특정 상태만 변화 감지

--- nomad-react-beginner/react-for-beginner/src/App.js
+++ nomad-react-beginner/react-for-beginner/src/App.js
@@ -4,21 +4,41 @@ import {useState, useEffect} from "react";
 function App() {
   const [counter, setCounter] = useState(0);
   const [result, setResult] = useState("doing...");
+  const [keyword, setKeyword] = useState("");
 
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
 
   console.log("i run all the time");
 
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("i run only once");
     setTimeout(() => {
       setResult("done!");
-      console.log("DONE");
+      console.log("i run only once, timer");
     }, 2000);
   }, []);
 
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("i run when 'keyword' & 'counter' changes");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
       <h3>{result}</h3>
