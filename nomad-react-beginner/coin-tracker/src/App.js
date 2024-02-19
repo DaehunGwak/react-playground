db5cc9d6c27c275efab1772736e082a9fdbfd55c
DaehunGwak <rhkrgns56@naver.com>
2024-02-19 23:40:22 +0900

    nomad-movie: movie - coin tracker 컨버터 챌린지 추가

--- nomad-react-beginner/coin-tracker/src/App.js
+++ nomad-react-beginner/coin-tracker/src/App.js
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState({});
+  const [usd, setUsd] = useState("");
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -10,25 +12,44 @@ function App() {
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setSelectedCoin(json[0]);
       });
   }, []);
+
+  const onSelectedCoinChange = (event) => setSelectedCoin(coins[event.target.value]);
+
+  const onUsdChange = (event) => setUsd(event.target.value);
 
   return (
     <div>
       <h1>The Coins! ({coins.length})</h1>
       {loading
         ? <strong>Loading</strong>
-        : <select>
-          {
-            coins.map((coin) => (
-              <option>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-              </option>
-            ))
-          }
-        </select>
+        : <div>
+          <select onChange={onSelectedCoinChange}>
+            {
+              coins.map((coin, index) => (
+                <option value={index} key={coin.id}>
+                  {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                </option>
+              ))
+            }
+          </select>
+          <hr/>
+          <div>
+            <input
+              value={usd}
+              onChange={onUsdChange}
+              type="number"
+              disabled={loading}
+              placeholder="변환을 원하시는 USD를 입력해주세요."
+            />
+          </div>
+          <div>
+            <strong>{selectedCoin.name} ({selectedCoin.symbol}): {usd / selectedCoin.quotes.USD.price}</strong>
+          </div>
+        </div>
       }
-
     </div>
   );
 }
