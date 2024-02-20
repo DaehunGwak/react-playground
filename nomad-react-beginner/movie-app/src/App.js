521c96afe527f5e3814742d37476dba5eb5fb7e2
DaehunGwak <rhkrgns56@naver.com>
2024-02-21 01:49:29 +0900

    nomad-movie: movie - movie app react-router gh-pages 적용

--- nomad-react-beginner/movie-app/src/App.js
+++ nomad-react-beginner/movie-app/src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
