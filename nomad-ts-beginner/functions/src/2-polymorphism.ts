0bafe02b4d6e5165377fde34b9b51ed18619107a
DaehunGwak <rhkrgns56@naver.com>
2024-02-24 02:42:46 +0900

    nomad-ts: functions - more generic 예제 추가

--- nomad-ts-beginner/functions/src/2-polymorphism.ts
+++ nomad-ts-beginner/functions/src/2-polymorphism.ts
@@ -14,8 +14,35 @@ superPrint(["a", "b", "c"]);
 superPrint([1, 2, true, false]);
 
 
-type TakeOne = <T>(arr: T[]) => T;
-
-const takeOne: TakeOne = (arr) => arr[0];
+function takeOne<T>(arr: T[]): T {
+  return arr[0];
+}
 
 console.log(takeOne([3, 4]));
+
+
+type Phone<T> = {
+  name: string,
+  extraInfo?: T,
+};
+
+type GalaxyExtra = {
+  version: string,
+};
+
+const galaxy: Phone<GalaxyExtra> = {
+  name: "Galaxy S22",
+  extraInfo: {
+    version: "1.2.0003",
+    // another: 123, // unknown property error
+  }
+};
+
+const customPhone: Phone<null> = {
+  name: "MyCustom",
+};
+
+
+type arrNumbers = Array<number>; // ts에 기본적으로 선언된 `interface Array<T>`
+// react useState 도 제네릭으로 구현되어 있음
+
