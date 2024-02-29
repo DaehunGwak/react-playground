dd5aa80f1baf8b1c939b0cf52929ddd278ccb111
DaehunGwak <rhkrgns56@naver.com>
2024-03-01 03:57:46 +0900

    nomad-ts: classes - default 클래스 예제 추가

--- nomad-ts-beginner/classes/src/0-classes.ts
+++ nomad-ts-beginner/classes/src/0-classes.ts
@@ -1,4 +1,28 @@
-class Player {
-  constructor() {
+abstract class User {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+    protected nickname: string,
+    public age: number
+  ) {}
+
+  abstract getNickName(): string
+
+  // 아무것도 없으면 public, private/protected 키워드 가능
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
+
+class Player extends User {
+  getNickName() {
+    return this.nickname;
+  }
+
+}
+
+const daehun = new Player('daehun', 'gwak', "Daehun", 30);
+
+// daehun.firstName // private error
+console.log(daehun.getNickName());
+console.log(daehun.getFullName());
