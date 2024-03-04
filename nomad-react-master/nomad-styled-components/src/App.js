353bd59d936a6f814d3529f01fac9bed12b152a2
DaehunGwak <rhkrgns56@naver.com>
2024-03-04 22:47:48 +0900

    nomad-react-master: styled-components - tag 변경(as) & attributes

--- nomad-react-master/nomad-styled-components/src/App.js
+++ nomad-react-master/nomad-styled-components/src/App.js
@@ -1,11 +1,13 @@
 import FirstComponent from "./components/FirstComponent";
 import AdaptingExtending from "./components/AdaptingExtending";
+import AsAndAttrs from "./components/AsAndAttrs";
 
 function App() {
   return (
     <div>
       <FirstComponent />
       <AdaptingExtending />
+      <AsAndAttrs />
     </div>
   );
 }
