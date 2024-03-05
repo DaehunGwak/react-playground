cbaf61f1cdaf0ed4241a9593e7cfc16691511234
DaehunGwak <rhkrgns56@naver.com>
2024-03-05 23:42:53 +0900

    nomad-react-master: ts - init project

---
+++ nomad-react-master/nomad-react-ts/README.md
@@ -0,0 +1,10 @@
+# nomad react master: 3. typescript
+
+## setup
+
+> 따로 `@types/styled-components` 관련 이슈는 없었음
+
+```shell
+npx create-react-app nomad-react-ts --template typescript 
+```
+
---
+++ nomad-react-master/nomad-react-ts/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StyledComponentsIntegration from "./components/StyledComponentsIntegration";
+
+function App() {
+  return (
+    <StyledComponentsIntegration />
+  );
+}
+
+export default App;
