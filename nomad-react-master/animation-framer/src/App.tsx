6832971e84864422fd408c35bf5653382a59ae53
DaehunGwak <rhkrgns56@naver.com>
2024-03-14 01:09:02 +0900

    nomad-react-ma: framer - init project

---
+++ nomad-react-master/animation-framer/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Wrapper} from "./styles/AppStyles";
+
+function App() {
+  return (
+    <Wrapper>
+      <Box/>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ nomad-react-master/animation-framer/src/styles/AppStyles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.2;
+  color: black;
+  background: linear-gradient(135deg,tomato,#ff9999);
+`;
+
+export const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 5px 5px 2px rgba(0, 0, 0, 0.1);
+`;
