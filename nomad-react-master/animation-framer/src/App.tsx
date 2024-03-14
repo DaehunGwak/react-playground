7253c903cecf06273297f09a57af3c5d6edf53b2
DaehunGwak <rhkrgns56@naver.com>
2024-03-14 22:59:25 +0900

    nomad-react-ma: framer - Variant (애니메이션 스테이지 개념)

--- nomad-react-master/animation-framer/src/App.tsx
+++ nomad-react-master/animation-framer/src/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import {Box, Wrapper} from "./styles/AppStyles";
 
+const myVariants = {
+  start: { scale: 0 },
+  end: { // 이름 맘대로 가능
+    scale: 1,
+    rotateZ: 360,
+    transition: {
+      delay: 1,
+      duration: 1,
+      type: "spring",
+      bounce: 0.25,
+    }
+  }
+}
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        animate={{
-          scale: 1,
-          rotateZ: 360
-        }}
-        transition={{
-          delay: 1,
-          duration: 1,
-          type: "spring",
-          bounce: 0.25,
-        }}
-      />
+      <Box variants={myVariants} initial="start" animate="end"/>
     </Wrapper>
   );
 }
