a93c4ef62d8d7e8703ca97db136ca24d6b258e90
DaehunGwak <rhkrgns56@naver.com>
2024-03-27 02:19:48 +0900

    nomad-next-beginner: 2. routing - defining routes

---
+++ nomad-next-beginner/first-next/app/about-us/page.tsx
@@ -0,0 +1,6 @@
+// path: /about-us
+export default function AboutUsPage() {
+  return (
+    <h2>about us</h2>
+  );
+}
--- nomad-next-beginner/first-next/app/page.tsx
+++ nomad-next-beginner/first-next/app/page.tsx
@@ -1,5 +1,11 @@
+// path: /
+import Avatar from "./components/avatar";
+
 export default function RootPage() {
   return (
-    <h1>hello next.js</h1>
+    <>
+      <h1>hello next.js</h1>
+      <Avatar/>
+    </>
   );
 }
