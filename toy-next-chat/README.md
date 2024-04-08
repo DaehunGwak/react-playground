0ed6939c11385e55a77f4c9da7ef9ae9b72ce2ff
DaehunGwak <rhkrgns56@naver.com>
2024-04-08 23:04:37 +0900

    toy-next-chat: Init project

---
+++ toy-next-chat/README.md
@@ -0,0 +1,17 @@
+# toy-next-chat
+
+## 목표
+
+- next.js, prisma, supabase 적응
+- polling, long-polling, Server Sent Event, websocket 적응 및 장단점 분석
+- (optional) redis pub/sub 구현
+
+## How to set up
+
+### Prisma + Supabase
+
+#### Prisma
+
+```sh
+npx prisma init
+```
---
+++ toy-next-chat/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <div className="rounded-3xl shadow-lg p-6 bg-white">
+        hello next.js chat
+      </div>
+    </main>
+  );
+}
