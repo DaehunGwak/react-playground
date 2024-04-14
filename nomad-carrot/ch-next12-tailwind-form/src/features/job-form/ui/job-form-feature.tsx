e7125f15aea0206d4de485b8bf1c9a6f897ef59f
DaehunGwak <rhkrgns56@naver.com>
2024-04-14 22:43:20 +0900

    nomad-carrot-ch: 54. tailwind form - Done form ui

--- nomad-carrot/ch-next12-tailwind-form/src/features/job-form/ui/job-form-feature.tsx
+++ nomad-carrot/ch-next12-tailwind-form/src/features/job-form/ui/job-form-feature.tsx
@@ -11,17 +11,57 @@ export default function JobFormFeature() {
           </div>
           <div className="form-option-wrapper">
             <input type="radio" name="department" value="marketing" id="departmentMarketing"/>
-            <label htmlFor="departmentSales" className="form-option-text">Marketing</label>
+            <label htmlFor="departmentMarketing" className="form-option-text">Marketing</label>
           </div>
           <div className="form-option-wrapper">
             <input type="radio" name="department" value="accounting" id="departmentAccounting"/>
-            <label htmlFor="departmentSales" className="form-option-text">Accounting</label>
+            <label htmlFor="departmentAccounting" className="form-option-text">Accounting</label>
           </div>
           <div className="form-option-wrapper">
             <input type="radio" name="department" value="customer-service" id="departmentCustomerService"/>
-            <label htmlFor="departmentSales" className="form-option-text">Customer Service</label>
+            <label htmlFor="departmentCustomerService" className="form-option-text">Customer Service</label>
           </div>
         </fieldset>
+        <fieldset className="form-question">
+          <legend className="form-description-text">Why do you want to join this company?</legend>
+          <div className="form-option-wrapper">
+            <input type="radio" name="purpose" value="money" id="purposeMoney"/>
+            <label htmlFor="purposeMoney" className="form-option-text">I want money!</label>
+          </div>
+          <div className="form-option-wrapper">
+            <input type="radio" name="purpose" value="love-company" id="purposeLoveCompany"/>
+            <label htmlFor="purposeLoveCompany" className="form-option-text">I love this company!</label>
+          </div>
+          <div className="form-option-wrapper">
+            <input type="radio" name="purpose" value="learn" id="purposeLearn"/>
+            <label htmlFor="purposeLearn" className="form-option-text">I want to learn</label>
+          </div>
+          <div className="form-option-wrapper">
+            <input type="radio" name="purpose" value="dont-know-why" id="purposeDontKnowWhy"/>
+            <label htmlFor="purposeDontKnowWhy" className="form-option-text">I don&apos;t know why</label>
+          </div>
+        </fieldset>
+        <div className="form-question">
+          <label htmlFor="salary-select" className="form-description-text">Salary</label>
+          <select id="salary-select" className="form-input">
+            <option value="$50K" defaultChecked>$50K</option>
+            <option value="$100K">$100K</option>
+            <option value="$150K">$150K</option>
+            <option value="$200K">$200K</option>
+          </select>
+        </div>
+        <div className="form-question">
+          <label htmlFor="introduceInput" className="form-description-text">Introduce yourself</label>
+          <input type="text" className="form-input" id="introduceInput"/>
+        </div>
+        <div className="form-question">
+          <label htmlFor="dreamsInput" className="form-description-text">Tell us what your dreams are</label>
+          <textarea className="form-input h-24" id="dreamsInput"/>
+        </div>
+        <div className="form-question">
+          <label htmlFor="emailInput" className="form-description-text">Email</label>
+          <input type="email" className="form-input" id="emailInput"/>
+        </div>
         <button type="submit" className="form-submit-button">Give me this job</button>
       </form>
     </div>
