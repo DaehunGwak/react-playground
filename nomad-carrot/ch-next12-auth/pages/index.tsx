7a1bdf3d819827d6f2dd8f8a64f7deecf99a36aa
DaehunGwak <rhkrgns56@naver.com>
2024-04-16 02:02:21 +0900

    nomad-carrot-ch: 57. auth - Init project

---
+++ nomad-carrot/ch-next12-auth/pages/index.tsx
@@ -0,0 +1,20 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Auth!: Nomad Carrot</title>
+        <meta name="description" content="Nomadcoders react challenge day 56 (next12, swr, " />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>hello world!</div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
